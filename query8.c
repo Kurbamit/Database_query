16969b0ed172cde7e94392e40a6dbe04cbf59eb7
Dominykas <113939016+Kurbamit@users.noreply.github.com>
2022-09-28 20:38:26 +0300

    Add files via upload

---
+++ query8.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct Student {
+	char name[30];
+	char surname[30];
+	int course;					// year of study
+	double average; 			// average grade
+	
+	int load; 					// number of courses
+	char courses[10][30]; 		// course names
+	int grades[10]; 			// course grades
+    
+	
+	char languages[100];		// spoken languages
+	
+    
+    int negative;    
+} Student;
+
+
+
+int main(int argc, char *argv[]) {
+	FILE *db = NULL;
+	// open database file for reading, provide a parameter or use default "db.bin"
+	if (argc > 1)
+		db = fopen(argv[1], "rb");
+	else
+		db = fopen("db.bin", "rb");
+		
+	if (db){							
+		Student students[1000];			// all the data goes here
+		int size = 0;					// how many students in database
+		
+		// reading data from file
+		fread(&size, sizeof(int), 1, db);
+		
+		for (int i = 0; i < size; i++){			
+			fread(&students[i], sizeof(Student), 1, db);			
+		}	
+		printf("%d records loaded succesfully\n", size);
+		
+		
+		// MODIFY CODE BELOW
+		
+		int counterDemo = 0; // for counting students
+
+		for (int i = 0; i < size; ++i){ // process all the student records in database
+			Student s = students[i]; // store data for each student in s
+            s.negative = 0;
+			// Checking if student has an Algebra and a Calculus
+			for(int j = 0; j < s.load; ++j)
+			{
+                for(int k = 0; k < s.load; ++k)
+                {
+                    if(!strcmp(s.courses[j], "Algebra") && !strcmp(s.courses[k], "Calculus"))
+                    {
+                        s.negative++;
+                    }
+                }
+			}
+            bool calculus_and_algebra = false;
+            if(s.negative != 0)    // If student has calculus and algebra, bool value will be set to true
+            {
+                calculus_and_algebra = true;
+            }
+
+			if(calculus_and_algebra){ // *** first filter, conditions on the student
+				int anotherDemo = 0; // for counting courses/grades
+				printf("%s %s %d %.2f %d ", s.name, s.surname, s.course, s.average, s.load); // print student data
+
+				for (int i = 0; i < s.load; ++i){ // process each course taken by the student
+					if(1){ // *** second filter, conditions on the course/grade
+						++anotherDemo; // counting courses
+						printf("[%s] %d ", s.courses[i], s.grades[i]);
+					}
+				}
+				printf("%s\n", s.languages);
+                
+				if (anotherDemo == s.load) // *** third filter, various other conditions			
+					++counterDemo; // counting studfents
+			}
+		}
+		printf("Filter applied, %d students found\n", counterDemo); // how many passed the filters
+		fclose(db);	
+	} else {
+    	printf("File db.bin not found, check current folder\n");
+	}
+
+	return 0;
+}
